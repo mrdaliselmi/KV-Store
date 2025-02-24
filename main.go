0610d703d510a79e117f486e7305f2c394ce7ef4
MohamedAliSelmi <mohamedali.selmi@insat.u-carthage.tn>
2025-02-24 11:17:02 +0100

    ✨ feat: implement AOF persistence with read and write functionality

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    fmt.Println("Listening on port :6379")
+	fmt.Println("Listening on port :6379")
 
 	// Create a new server
 	l, err := net.Listen("tcp", ":6379")
@@ -15,6 +15,26 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
 
 	// Listen for connections
 	conn, err := l.Accept()
@@ -54,7 +74,11 @@ func main() {
 			writer.Write(Value{typ: "string", str: ""})
 			continue
 		}
-	
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
+
 		result := handler(args)
 		writer.Write(result)
 	}
